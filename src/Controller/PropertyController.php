5f9e71cbf5e5335f9baf59cc59ce3816b0b50b7b
SebAvenel <sebastien.avenel@outlook.fr>
2022-05-25 17:10:28 +0200

    création du contrôleur 'PropertyControlleur'

---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PropertyController extends AbstractController
+{
+    /**
+     * @Route("/biens", name="app_property")
+     */
+    public function index(): Response
+    {
+        return $this->render('property/index.html.twig', [
+            'controller_name' => 'PropertyController',
+        ]);
+    }
+}
