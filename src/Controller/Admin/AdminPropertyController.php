f75b6a58975a9594ea47c7c54c2de0d3d9d7a1a3
SebAvenel <sebastien.avenel@outlook.fr>
2022-06-23 18:03:18 +0200

    Gestion de l'enregistrement des modification dans le contrôleur

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -5,7 +5,9 @@ namespace App\Controller\Admin;
 use App\Entity\Property;
 use App\Form\PropertyType;
 use App\Repository\PropertyRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,9 +18,16 @@ class AdminPropertyController extends AbstractController
      */
     private $repository;
 
-    public function __construct(PropertyRepository $repository)
+    /**
+     * @var ObjectManager
+     *
+     */
+    private $em;
+
+    public function __construct(PropertyRepository $repository, EntityManagerInterface $em)
     {
         $this->repository = $repository;
+        $this->em = $em;
     }
 
     /**
@@ -37,9 +46,14 @@ class AdminPropertyController extends AbstractController
      * @Route("/admin/edit/{id}", name="admin.property.edit")
      * @return Response
      */
-    public function edit(Property $property): Response
+    public function edit(Property $property, Request $request): Response
     {
         $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->flush();
+            return $this->redirectToRoute('admin.property.index');
+        }
         return $this->render('admin/edit.html.twig', [
             'property' => $property,
             'form' => $form->createView()
