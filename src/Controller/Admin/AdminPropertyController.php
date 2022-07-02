e2f30f1ad85427230396b0317cb4cc3bc6750240
SebAvenel <sebastien.avenel@outlook.fr>
2022-07-02 18:14:21 +0200

    fin de la fonctionnalité de modification des bien

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Property;
 use App\Form\PropertyType;
 use App\Repository\PropertyRepository;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,7 +20,7 @@ class AdminPropertyController extends AbstractController
     private $repository;
 
     /**
-     * @var ObjectManager
+     * @var EntityManagerInterface
      *
      */
     private $em;
@@ -31,19 +32,38 @@ class AdminPropertyController extends AbstractController
     }
 
     /**
-     * @Route("/admin", name="admin.property.index")
+     * @Route("/admin/property", name="admin.property.index")
      * @return Response
      */
     public function index(): Response
     {
         $properties = $this->repository->findAll();
-        return $this->render('admin/index.html.twig', [
+        return $this->render('admin/property/index.html.twig', [
             'properties' => $properties
         ]);
     }
 
     /**
-     * @Route("/admin/edit/{id}", name="admin.property.edit")
+     * @Route("/admin/property/create", name="admin.property.create")
+     * @return Response
+     */
+    public function new(Request $request): Response
+    {
+        $property = new Property();
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($property);
+            $this->em->flush();
+            return $this->redirectToRoute('admin.property.index');
+        }
+        return $this->render('admin/property/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/property/edit/{id}", name="admin.property.edit")
      * @return Response
      */
     public function edit(Property $property, Request $request): Response
@@ -54,7 +74,7 @@ class AdminPropertyController extends AbstractController
             $this->em->flush();
             return $this->redirectToRoute('admin.property.index');
         }
-        return $this->render('admin/edit.html.twig', [
+        return $this->render('admin/property/edit.html.twig', [
             'property' => $property,
             'form' => $form->createView()
         ]);
