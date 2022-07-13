5c2d6f0c9e0bad4c1e02ee4f173010120c34c46e
SebAvenel <sebastien.avenel@outlook.fr>
2022-07-13 22:58:52 +0200

    création de l'entité de test (2 valeurs) qui va nous servir à générer le formulaire de filtrage

---
+++ src/Entity/PropertySearch.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity;
+
+
+class PropertySearch
+{
+    /**
+     * @var int|null
+     */
+    public $maxValue;
+
+    /**
+     * @var int|null
+     */
+    public $minSurface;
+
+
+
+    /**
+     * Get the value of maxValue
+     */
+    public function getMaxValue(): ?int
+    {
+        return $this->maxValue;
+    }
+
+    /**
+     * Set the value of maxValue
+     */
+    public function setMaxValue(?int $maxValue): self
+    {
+        $this->maxValue = $maxValue;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of minSurface
+     */
+    public function getMinSurface(): ?int
+    {
+        return $this->minSurface;
+    }
+
+    /**
+     * Set the value of minSurface
+     */
+    public function setMinSurface(?int $minSurface): self
+    {
+        $this->minSurface = $minSurface;
+
+        return $this;
+    }
+}
