0afb1f4d11b6bfb77eac87bb0593694164033c18
Sébastien avenel <sebastien.avenel@outlook.fr>
2023-01-04 20:52:16 +0100

    ajout des contrôles d'accés

--- src/Controller/Admin/AdminUserController.php
+++ src/Controller/Admin/AdminUserController.php
@@ -13,9 +13,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/admin/user")
+ * @IsGranted("ROLE_ADMIN")
  */
 class AdminUserController extends AbstractController
 {
